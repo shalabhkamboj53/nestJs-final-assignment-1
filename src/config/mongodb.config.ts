401969b902956fd06f53e7e9dfa56fd2e02025e8
Shalabh Kamboj <shalabh.kamboj@appinventiv.com>
2026-05-12 10:39:26 +0530

    linting fixes

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -2,5 +2,5 @@
 // This file exports the MongoDB connection configuration using environment variables
 
 export const mongodbConfig = () => ({
-  uri: process.env.MONGODB_URI,
+  uri: process.env.MONGODB_URI || '',
 });
